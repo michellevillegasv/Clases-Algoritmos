bbe7f76f8e00ff037b3c39dd6245447127fec2bf
michellevillegasss <michelle.villegas@correo.unimet.edu.ve>
2022-09-21 10:16:26 -0400

    Ejercicio 2 ciclos :poop:

---
+++ Ejercicios Clases/Ejercicio 2 ciclos.py
@@ -0,0 +1,11 @@
+number = int(input("Please neter a number: "))
+aux = number -1
+acum = 0 
+while aux >= 1: 
+    if number % aux == 0:
+        acum += aux
+    aux-=1
+if acum > number:
+    print(f"The number {number} is abundant")
+else:
+    print(f"The number {number} is not abundant")
